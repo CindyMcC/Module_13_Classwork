f39e6454e8933808ca2a60996b2b6105e69d98c4
unknown <cm0406@gmail.com>
2021-09-28 14:28:39 -0400

    update index.html

---
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -0,0 +1,61 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+// this shows the US
+// let map = L.map('mapid').setView([40.7, -94.5], 4);
+// this shows a zoom into Los Angeles
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+
+// alternative method to using setView
+// Create the map object with a center and zoom level.
+// let map = L.map("mapid", {
+//     center: [
+//       40.7, -94.5
+//     ],
+//     zoom: 4
+//   });
+
+//  Add a marker to the map for Los Angeles, California.
+// this adds a plain marker
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// this changes the marker to a circle
+// L.circle([34.0522, -118.2437], {
+//     radius: 100
+//  }).addTo(map);
+ // this changes the marker to a light-yellow circle with black lines,
+ // indicating a 300-pixel radius
+ L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+ }).addTo(map);
+
+// ADD A TILE LAYER FOR OUR MAP - 2 METHODS
+// METHOD 1: USE THE LEAFLET DOCUMENTATION
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    // id: 'mapbox/streets-v11',  //street view
+    id: 'mapbox/dark-v10',  //dark map
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+// METHOD 2: USE THE MAPBOX STYLES API
+// We create the tile layer that will be the background of our map.
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
+
+// // Then we add our 'graymap' tile layer to the map.
+// streets.addTo(map);
+
+
+
---
+++ Mapping_Single_Points/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiY2luZHltY2MiLCJhIjoiY2t0c3F2am1oMTlnZjMwbzE4amRkcTV3NCJ9.zclTsGaMansT2kGa7LlP7Q";
